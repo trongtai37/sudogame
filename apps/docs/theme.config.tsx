93e1742bb779ef2b2a66913a7b8d2f4e062931d7
Tai Nguyen Trong <trongtaind37@gmail.com>
2023-07-12 22:10:56 +0700

    feat: add analytics component

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
+import { Analytics } from '@vercel/analytics/react';
+import { useRouter } from 'next/router';
 
 const config: DocsThemeConfig = {
-  logo: <span>SUDOgame</span>,
+  logo: (
+    <span
+      style={{
+        fontWeight: 500,
+      }}
+    >
+      SUDO | game
+    </span>
+  ),
   project: {
     link: 'https://github.com/trongtai37/sudogame',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s - Sudogame',
+      };
+    }
+    return {
+      titleTemplate:
+        'Sudogame - A new fast, light and comprehensive Sudoku library, powered by SAT encoding!',
+    };
+  },
+  footer: {
+    component: <Analytics />,
+  },
 };
 
 export default config;
